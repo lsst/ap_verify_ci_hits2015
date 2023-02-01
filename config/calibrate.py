0d54c885bbc2337cde36d90699c6bf645cb24b3c
Krzysztof Findeisen <kfindeis@uw.edu>
2023-02-01 11:43:40 -0800

    Fix deprecation	warning	from calibrate config.
    The Gen	2 refcat config	is no longer required, and will	break the
    build after Pipelines release 26.

--- config/calibrate.py
+++ config/calibrate.py
@@ -3,12 +3,10 @@
 # Use gaia for astrometry (phot_g_mean for everything, as that is the broadest
 # band with the most depth)
 config.connections.astromRefCat = "gaia"
-config.astromRefObjLoader.ref_dataset_name = config.connections.astromRefCat
 config.astromRefObjLoader.anyFilterMapsToThis = "phot_g_mean"
 
 # Use panstarrs for photometry (grizy filters)
 config.connections.photoRefCat = "panstarrs"
-config.photoRefObjLoader.ref_dataset_name = config.connections.photoRefCat
 config.photoRefObjLoader.filterMap = {
     "u": "g",
     "VR": "g"}
