707b549f9f09711bd1a407143c59081c0304bae9
Krzysztof Findeisen <kfindeis@uw.edu>
2023-04-25 17:57:33 -0700

    Remove refcat renaming and update preloaded/ accordingly.
    The original Gen 2 dataset and `scripts/generate_refcats_gen3.py`
    renamed their refcats to ensure that ap_verify runs always used the
    dataset's own refcats rather than the "global" copy on lsst-dev. In
    Gen 3, refcats are read from specific collections, so this safeguard
    adds unnecessary complexity and prevents the use of some Butler tools.

--- config/calibrate.py
+++ config/calibrate.py
@@ -1,12 +1,14 @@
 # Config override for lsst.pipe.tasks.calibrate.CalibrateTask
+# Ensure we're using the refcats that are actually in this dataset, regardless
+# of what task defaults are.
 
 # Use gaia for astrometry (phot_g_mean for everything, as that is the broadest
 # band with the most depth)
-config.connections.astromRefCat = "gaia"
+config.connections.astromRefCat = "gaia_dr2_20200414"
 config.astromRefObjLoader.anyFilterMapsToThis = "phot_g_mean"
 
 # Use panstarrs for photometry (grizy filters)
-config.connections.photoRefCat = "panstarrs"
+config.connections.photoRefCat = "ps1_pv3_3pi_20170110"
 config.photoRefObjLoader.filterMap = {
     "u": "g",
     "VR": "g"}
