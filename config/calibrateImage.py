d0928dcb48368e852860904cd408b6fbec1529c4
John Parejko <parejkoj@uw.edu>
2023-08-15 13:39:08 -0700

    Add pipeline files for new CalibrateImageTask

--- config/calibrateImage.py
+++ config/calibrateImage.py
@@ -1,15 +1,14 @@
-# TODO DM-40388: Remove this file, once the new pipeline is vetted.
 # Config override for lsst.pipe.tasks.calibrate.CalibrateTask
 # Ensure we're using the refcats that are actually in this dataset, regardless
 # of what task defaults are.
 
 # Use gaia for astrometry (phot_g_mean for everything, as that is the broadest
 # band with the most depth)
-config.connections.astromRefCat = "gaia_dr2_20200414"
-config.astromRefObjLoader.anyFilterMapsToThis = "phot_g_mean"
+config.connections.astrometry_ref_cat = "gaia_dr2_20200414"
+config.astrometry_ref_loader.anyFilterMapsToThis = "phot_g_mean"
 
 # Use panstarrs for photometry (grizy filters)
-config.connections.photoRefCat = "ps1_pv3_3pi_20170110"
-config.photoRefObjLoader.filterMap = {
+config.connections.photometry_ref_cat = "ps1_pv3_3pi_20170110"
+config.photometry_ref_loader.filterMap = {
     "u": "g",
     "VR": "g"}
