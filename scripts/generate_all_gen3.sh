e443163d260e836642d63298a917882b42f5ca02
BrunoSanchez <bsanchez@cppm.in2p3.fr>
2024-07-23 10:59:48 -0700

    Style fix

--- scripts/generate_all_gen3.sh
+++ scripts/generate_all_gen3.sh
@@ -128,7 +128,7 @@ python "${SCRIPT_DIR}/generate_ephemerides_gen3.py"
 ########################################
 # Final clean-up
 
-butler collection-chain "${DATASET_REPO}" sso sso/cached 
+butler collection-chain "${DATASET_REPO}" sso sso/cached
 butler collection-chain "${DATASET_REPO}" DECam/defaults templates/goodSeeing skymaps DECam/calib refcats sso \
     models ${INJECTION_CATALOG_COLLECTION}
 python "${SCRIPT_DIR}/make_preloaded_export.py" --dataset ap_verify_ci_hits2015
